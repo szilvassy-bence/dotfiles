4145057ccadf9710630471f0913f3bf3b2d3e2c8
szilvassy-bence <szilvassy.bence@pepita.hu>
2024-11-10 16:37:58 +0100

    nvim tree settings

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -2,6 +2,11 @@ require('nvim-tree').setup({
   git = {
     ignore = false,
   },
+  actions = {
+    open_file = {
+      quit_on_open = true, -- This closes the tree when a file is opened.
+    },
+  },
   renderer = {
     group_empty = true,
     icons = {
