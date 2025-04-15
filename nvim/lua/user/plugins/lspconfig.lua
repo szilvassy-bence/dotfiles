c08b96760ff986aac1086f50da6116a4eabed6f8
bszilvassy <szilvassy.bence@pepita.hu>
2025-04-15 20:02:07 +0200

    which-key

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -12,10 +12,6 @@ require('lspconfig').intelephense.setup({
 require('lspconfig').phpactor.setup({
   capabilities = capabilities,
   on_attach = on_attach,
-  init_options = {
-    ["language_server_phpstan.enabled"] = false,
-    ["language_server_psalm.enabled"] = false,
-  },
   filetypes = { 'php' },
 })
 
